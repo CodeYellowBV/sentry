3831ff4cecf8c96725177b9798ef149876230cd2
Stefan <stefan@codeyellow.nl>
2014-08-16 15:23:32 +0200

    Added documentation

--- src/Cartalyst/Sentry/SessionHandlers/NativeSessionHandler.php
+++ src/Cartalyst/Sentry/SessionHandlers/NativeSessionHandler.php
@@ -3,9 +3,26 @@ use Cartalyst\Sentry\Cookies\CookieInterface;
 use Cartalyst\Sentry\Cookies\NativeCookie;
 use Cartalyst\Sentry\Sessions\NativeSession;
 use Cartalyst\Sentry\Sessions\SessionInterface;
+
 /**
- * General session class. Will handle sessions more easily
+ * Part of the Sentry package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.  It is also available at
+ * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ * @package    Sentry
+ * @version    2.0.0
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011 - 2013, Cartalyst LLC
+ * @link       http://cartalyst.com
  */
+
 class NativeSessionHandler implements SessionHandlerInterface
 {
 	/**
@@ -22,8 +39,12 @@ class NativeSessionHandler implements SessionHandlerInterface
 	 */
 	protected $cookie;
 
-
-
+	/**
+	 * Construct a new SessionHandler
+	 *
+	 * @param Cartalyst\Sentry\Sessions\SessionInterface $session The sessiondriver
+	 * @param Cartalyst\Sentry\Cookies\CookieInterface $cookie The CookieInterface
+	 */
 	public function __construct(
 		SessionInterface $session = null,
 		CookieInterface $cookie = null
--- src/Cartalyst/Sentry/SessionHandlers/SessionHandlerInterface.php
+++ src/Cartalyst/Sentry/SessionHandlers/SessionHandlerInterface.php
@@ -3,24 +3,72 @@ use Cartalyst\Sentry\Cookies\CookieInterface;
 use Cartalyst\Sentry\Cookies\NativeCookie;
 use Cartalyst\Sentry\Sessions\NativeSession;
 use Cartalyst\Sentry\Sessions\SessionInterface;
+
 /**
- * General session class. Will handle sessions more easily
+ * Part of the Sentry package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.  It is also available at
+ * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ * @package    Sentry
+ * @version    2.0.0
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011 - 2013, Cartalyst LLC
+ * @link       http://cartalyst.com
  */
+
 interface SessionHandlerInterface
 {
 
 	/**
 	 * Sets a value in the session
+	 *
+	 * @param string $key The key of the value to be set
+	 * @param mixed $value The value to be set
+	 * @return void
 	 */
 	public function set($key, $value);
 
+	/**
+	 * Get a value in the session
+	 *
+	 * @param string $key The key of the value to get
+	 * @return void
+	 */
 	public function get($key);
 
+	/**
+	 * Completely destroy the session
+	 *
+	 * @return true;
+	 */
 	public function destroy();
 
+	/**
+	 * Set the session interface for this handler
+	 *
+	 * @param Cartalyst\Sentry\Sessions\SessionInterface $session The Sessioninterface to set
+	 * @return void
+	 */
 	public function setSession(SessionInterface $session);
 
+	/**
+	 * Get the sessiondriver
+	 *
+	 * @return Cartalyst\Sentry\Sessions\SessionInterface
+	 */
 	public function getSession();
 
+	/**
+	 * Make the session be kept forever
+	 *
+	 * @return void
+	 */
 	public function forever();
 }
